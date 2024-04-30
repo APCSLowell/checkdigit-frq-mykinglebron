b02faa646cd2da12e7aac59aefb1147cd2a1456c
Eric Zhao <140433074+mykinglebron@users.noreply.github.com>
2024-04-29 23:22:09 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -9,7 +9,7 @@ public class CheckDigit
    {  
      int sum=0; 
      for(int i=1; i<getNumberOfDigits(num); i++){
-        sum =sum + ((8-i) * getDigit(num, i));
+        sum =sum +( (8-i) * getDigit(num, i) );
      }
      return sum%10;
    }
@@ -26,7 +26,7 @@ public class CheckDigit
      int num= numWithCheckDigit / 10;
     int newCheck =getCheck(num);
    if(newCheck==check){
-return true)}
+return true;}
      else{return false;}
    }    
    
